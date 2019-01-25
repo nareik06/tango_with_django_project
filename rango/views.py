4259ae6d682e3cb515884d2061c47d831d477f7a
Kieran Swedlund <2324137s@student.gla.ac.uk>
2019-01-25 14:50:54 +0000

    end of chapter 4 (images, templates, media)

--- rango/views.py
+++ rango/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
+    #return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+     context_dict = {'boldmessage': "ABOUT!"}
+     return render(request, 'rango/about.html', context=context_dict)
+
+     #return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
