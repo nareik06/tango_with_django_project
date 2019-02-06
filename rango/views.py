7d376d239f65314848596f2ad14c173cd63a0652
Kieran Swedlund <2324137s@student.gla.ac.uk>
2019-02-06 14:00:16 +0000

    end of chapter 10

--- rango/views.py
+++ rango/views.py
@@ -8,27 +8,43 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout
+from datetime import datetime
+
 
 from django.http import HttpResponse
 
 def index(request):
+    request.session.set_test_cookie()
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views') [:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
-    return render(request, 'rango/index.html', context=context_dict)
 
-
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
     #return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
 
+    
 def about(request):
-     context_dict = {'boldmessage': "ABOUT!"}
-     return render(request, 'rango/about.html', context=context_dict)
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
+    context_dict = {'boldmessage': "ABOUT!"}
 
-     #return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    response = render(request, 'rango/about.html', context_dict)
+    return response
+
+    #return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
 
 def show_category(request, category_name_slug):
     context_dict = {}
@@ -205,7 +221,10 @@ def user_login(request):
 #login/logout
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    #return HttpResponse("Since you're logged in, you can see this text!")
+    context_dict = {'boldmessage': "RESTRICTED!"}
+    return render(request, 'rango/restricted.html', context=context_dict)
+
 
 @login_required
 def user_logout(request):
@@ -213,3 +232,35 @@ def user_logout(request):
     logout(request)
     # Take the user back to the homepage.
     return HttpResponseRedirect(reverse('index'))
+
+
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site.
+    # We use the COOKIES.get() function to obtain the visits cookie.
+    # If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                    '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
